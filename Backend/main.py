abacae3c1cb6ac87c0107c5174540e8736f68447
Senbhagamoorthi <23pca132@anjaconline.org>
2025-05-04 19:20:13 +0530

    hi

--- Backend/main.py
+++ Backend/main.py
@@ -37,7 +37,7 @@ def create_application():
     app = FastAPI()
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://cloudkitchenhub.netlify.app/"],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
